06500b102ccb666e54e8d4100e15b4b9bcd2fa0f
Andrey-Bazhanov <abazhanov@shakuro.com>
2015-04-20 15:58:03 +0300

    fb canvas fix

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -152,6 +152,20 @@ window.fbAsyncInit = function() {
     }
   });
 
+
+  FB.getLoginStatus(function(response) {
+      // Check login status on load, and if the user is
+      // already logged in, go directly to the welcome message.
+      if (response.status == 'connected') {
+        // onLogin(response);
+      } else {
+        // Otherwise, show Login dialog first.
+        FB.login(function(response) {
+          // onLogin(response);
+        }, {scope: 'user_friends, email'});
+      }
+  });
+
 };
 
 
